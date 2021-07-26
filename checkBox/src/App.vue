4589612e9340fc84de1e1272eb8803255cf6fafe
DenBotvinovskii <denis.botvinovsky@gmail.com>
2021-07-26 17:25:34 +0300

    v0.2

--- checkBox/src/App.vue
+++ checkBox/src/App.vue
@@ -1,13 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <h1>Форма подачи заявки в отдел сервиса и качества</h1>
+
+    <div class="form">
+      <div>Ваш филиал <span> *</span></div>
+      <label for="">Онлайн</label>
+      <select name="" id="">
+        <option selected>Выберите город</option>
+      </select>
+      <div>Тема обращения <span>*</span></div>
+      <input placeholder="Другое" type="text" />
+      <div>Описание проблемы <span>*</span></div>
+      <input type="text" />
+      <div>
+        Загрузка документов
+        <p>
+          Приложите, пожалуйста, полноэкранный скриншот. Это поможет быстрее
+          решить проблему.
+        </p>
+      </div>
+      <input type="file" />
+      <button>Отправить</button>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+//
 </script>
 
 <style>
@@ -17,6 +36,26 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+}
+.form {
+  width: 60%;
+  height: 100%; 
+  border: 1px solid #2c3e50; 
+  border-radius: 10px;
+  display: inline-block;
+}
+span {
+  color: orange;
+}
+button {
+  background-color: orange;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
 }
 </style>
